707673b5186c64fb930a1ddc8d2350bed0d4abef
sayid0927 <13113639501@163.com>
2018-11-29 00:33:30 +0800

    dd

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,19 +1,32 @@
 <template>
-<div class="body">
+  <div class="body">
 
-  <center>
+    <center>
 
-  </center>
+      <ul v-for="info in myInfo">
+        <input type="text" v-model="info.city" disabled readonly>
+        <input type="text" v-model="info.name" disabled readonly>
+      </ul>
+    </center>
 
-</div>
+  </div>
 </template>
-<script type="text/javascript">
-export default {
-  name: 'home'
-}
+<script>
+  export default {
+    name: 'home',
+    data () {
+      return {
+        title: '测试动态添加',
+        city: '',
+        name: '',
+        info: '',
+        myInfo: []
+      }
+    }
+  }
 </script>
 <style type="text/css">
-  .test{
+  .test {
     height: 40px;
     background: #ccc;
     margin-bottom: 15px;
